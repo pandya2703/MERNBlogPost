621b7929cbca9503656343ac85a8fff729cf7ea3
Hetanshu Pandya <hpan2703@gmail.com>
2023-05-14 16:08:09 -0400

    changed title

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          My Best Blog
+          My fav Blog is this one
         </a>
       </header>
     </div>
